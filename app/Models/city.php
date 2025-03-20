5ade51e42d4a417b13cfc2b9ee53ff24ac2a6a22
A <147518138+bedolls@users.noreply.github.com>
2025-03-20 15:19:38 +0700

    memasukkan data di city dan category

--- app/Models/city.php
+++ app/Models/city.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class city extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable =[
+        'image',
+        'name',
+        'slug'
+    ];
 }
